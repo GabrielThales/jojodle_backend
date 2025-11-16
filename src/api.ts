051a7bd31691bf590bdac3f0172c3c0c34aa546b
Gabriel Thales Alves da Silva <gabrielthalles7@gmail.com>
2025-11-16 16:33:34 -0300

    fix: whitelist, build vercel e CORS

--- src/api.ts
+++ src/api.ts
@@ -31,7 +31,7 @@ const app = express();
 
 // 2. Usar Middlewares
 // O seu frontend vai estar em, por exemplo, "https://meu-jojodle.com"
-const whitelist = ['https://jojodle-blond.vercel.app/'];
+const whitelist = ['https://jojodle-blond.vercel.app/', 'http://127.0.0.1:5500'];
 
 const corsOptions: CorsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
